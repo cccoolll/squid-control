42be0851d9ae2dee5d3020834c33ea67da956810
Songtao Cheng <36098954+cccoolll@users.noreply.github.com>
2024-05-24 16:49:40 +0200

    refactor: Update SquidController constructor to remove default value for is_simulation parameter

--- squid_control/squid_controller.py
+++ squid_control/squid_controller.py
@@ -15,7 +15,7 @@ import time
 class SquidController:
     fps_software_trigger= 100
 
-    def __init__(self,is_simulation = True, *args, **kwargs):
+    def __init__(self,is_simulation, *args, **kwargs):
         super().__init__(*args,**kwargs)
         self.data_channel = None
         #load objects
